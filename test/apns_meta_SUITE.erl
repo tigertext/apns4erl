3d56120b47fb50f31ec6ce170f56996d0f0f3509
Leo Wang <lwang@tigertext.com>
2018-07-17 14:28:14 +0800

    Rollback to inaka/apns4erl v2.2.0

--- test/apns_meta_SUITE.erl
+++ test/apns_meta_SUITE.erl
@@ -14,13 +14,7 @@
 -type config() :: [{atom(), term()}].
 
 -spec init_per_suite(config()) -> config().
-init_per_suite(Config) ->
-    [ {application, apns}
-        %% Until the next version of katana-test fixes the missing test deps in plt
-        %% %% issue, we can't use the default warnings that include 'unknown' here.
-        , {dialyzer_warnings, [error_handling, race_conditions, unmatched_returns]}
-        | Config
-    ].
+init_per_suite(Config) -> [{application, apns} | Config].
 
 -spec end_per_suite(config()) -> config().
 end_per_suite(Config) -> Config.
